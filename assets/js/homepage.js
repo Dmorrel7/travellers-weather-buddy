d78562a2f6fa1d0da553d10b75a858b2d06aabbc
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-11-22 16:07:48 -0500

    load/save function fixed and forecast display

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,10 @@
 var cityForm = document.querySelector("#city-form");
 var citySearch = document.querySelector("#city-search");
 var searchBtn = document.querySelector("#search-btn");
+var recentCityEl = document.querySelector("#recent-cities");
+var forecastDiv = document.querySelector("#forecastDiv")
+var citySave = new Array();
+
 
 var formHandler = function(event)
 {
@@ -10,8 +14,9 @@ var formHandler = function(event)
     if (cityName)
     {
         getCurrentWeather(cityName);
+        getForecast(cityName);
     }
-}
+};
 
 var getCurrentWeather = function(city)
 {
@@ -24,8 +29,9 @@ var getCurrentWeather = function(city)
             // parse the fetch to json format
             response.json().then(function(data)
             {
-                displayWeather(data)
-                console.log(data);
+                displayWeather(data);
+                saveCity(city);
+                loadCities();
             });
         }
     });
@@ -48,4 +54,83 @@ var displayWeather = function(data)
 
 };
 
+var getForecast = function()
+{
+    var forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=tampa&units=imperial&appid=af8fc128c23bcd35c1ad013855d92758"
+
+    fetch(forecastApiUrl).then(function(response)
+    {
+        if (response.ok)
+        {
+            response.json().then(function(data)
+            {
+                displayForecast(data);
+                console.log(data);
+            });
+        }
+    });
+};
+
+var displayForecast = function(data)
+{
+    for (var i = 1; i < 6; i++)
+    {
+        var forecastCol = document.createElement("div");
+        forecastCol.className = "col";
+        forecastDiv.appendChild(forecastCol);
+
+        var forecastCard = document.createElement("div");
+        forecastCard.className = "card";
+        forecastCol.appendChild(forecastCard);
+
+        var forecastDate = moment().add(i, 'd').format("MM/DD/YYYY");
+
+
+        var forecastDates = document.createElement("h5");
+        forecastDates.id = "date";
+        forecastDates.textContent = forecastDate;
+        forecastCard.appendChild(forecastDates);
+
+        var forecastTemp = document.createElement("p");
+        forecastTemp.id = "forecast-temp";
+        forecastTemp.textContent = "Temp: " + Math.round(data.list[i].main.temp) + " F";
+        forecastCard.appendChild(forecastTemp);
+
+        var forecastHumidity = document.createElement("p");
+        forecastHumidity.id = "forecast-Humidity";
+        forecastHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+        forecastCard.appendChild(forecastHumidity);
+    }
+};
+
+var saveCity = function(city)
+{
+    citySave.push(city);
+    localStorage.setItem("cities", JSON.stringify(citySave));
+};
+
+var loadCities = function()
+{
+    
+    recentCityEl.innerHTML = "";
+    citySave = JSON.parse(localStorage.getItem("cities"));
+
+    if (citySave && citySave.length > 0)
+    {
+        var newCities = [...new Set(citySave)];
+        for(var i = 0; i < newCities.length; i++)
+        {
+            var recentCities = document.createElement("button");
+            recentCities.className = "list-group-item";
+            recentCities.setAttribute("value", newCities[i]);
+            recentCities.textContent = newCities[i];
+            recentCityEl.appendChild(recentCities);
+        }
+    } 
+};
+
+if(localStorage.getItem("cities"))
+{
+    loadCities();
+}
 cityForm.addEventListener("submit", formHandler);
